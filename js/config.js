6eaa8713ee70d09745131a06f79babd0bf4f2fd0
Yanhao Ding <yding318@gatech.edu>
2020-07-12 20:17:00 -0700

    deploy

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://pg9pvek9p2.execute-api.us-east-2.amazonaws.com/pro' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
